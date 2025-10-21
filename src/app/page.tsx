7d160896bdfb646df33972b30dd5b7149a154612
oCaioAug <ocaioaug.dev@protonmail.com>
2025-10-21 19:48:36 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,45 @@
 import Image from "next/image";
 
 import Header from "@/components/common/header";
+import ProductList from "@/components/common/products-list";
+import { db } from "@/db";
 
-export default function Home() {
+export default async function Home() {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+      category: true,
+    },
+  });
+  console.log(products);
+
   return (
     <>
       <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList title="Featured Products" products={products} />
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 }
